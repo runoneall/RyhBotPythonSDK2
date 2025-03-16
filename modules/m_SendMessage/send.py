e008c93a971c8880f08443850a62d5657803f74f
runoneall <runoobsteve@hotmail.com>
2025-03-16 14:27:44 +0800

    update

--- modules/m_SendMessage/send.py
+++ modules/m_SendMessage/send.py
@@ -1,7 +1,24 @@
 class SendMessage:
     def __init__(self, sdk):
-        self.token = sdk.MessageBase.yhToken
+        self.apiUrl = (
+            "https://chat-go.jwzhd.com/open-apis/v1/bot/send?token="
+            + sdk.MessageBase.yhToken
+        )
         self.NetJsonPost = sdk.MessageBase.NetJsonPost
-    
-    def print_token(self):
-        print(self.token)
+        self.NetFileUpload = sdk.MessageBase.NetFileUpload
+
+    def _gen_body(
+        self,
+        recvId: str,
+        recvType: str,
+        contentType: str,
+        content: dict[str, any],
+        parentId: str,
+    ) -> dict[str, any]:
+        return {
+            "recvId": recvId,
+            "recvType": recvType,
+            "contentType": contentType,
+            "content": content,
+            "parentId": parentId,
+        }
