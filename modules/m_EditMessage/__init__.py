eef44fe4aea593854dcd7ff3af2bdd10d6a6a33b
runoneall <runoobsteve@hotmail.com>
2025-03-17 09:11:41 +0800

    update

---
+++ modules/m_EditMessage/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "EditMessage",
+    "version": "1.0.0",
+    "description": "云湖SDK消息编辑模块",
+    "author": "r1a",
+    "dependencies": ["m_MessageBase"],
+}
+
+from .edit import EditMessage as Main
---
+++ modules/m_EditMessage/edit.py
@@ -0,0 +1,61 @@
+class EditMessage:
+    def __init__(self, sdk):
+        self.yhToken = sdk.MessageBase.yhToken
+        self.apiUrl = (
+            "https://chat-go.jwzhd.com/open-apis/v1/bot/edit?token=" + self.yhToken
+        )
+        self.NetJsonPost = sdk.MessageBase.NetJsonPost
+
+    def _gen_body(
+        self,
+        msgId: str,
+        recvId: str,
+        recvType: str,
+        contentType: str,
+        content: dict[str, any],
+    ) -> dict[str, any]:
+        return {
+            "msgId": msgId,
+            "recvId": recvId,
+            "recvType": recvType,
+            "contentType": contentType,
+            "content": content,
+        }
+
+    def Text(
+        self,
+        msgId: str,
+        recvId: str,
+        recvType: str,
+        content: str,
+        buttons: list[list[dict[str, any]]] = [],
+    ) -> dict[str, any]:
+        return self.NetJsonPost(
+            self.apiUrl,
+            self._gen_body(
+                msgId,
+                recvId,
+                recvType,
+                "text",
+                {"text": content, "buttons": buttons},
+            ),
+        )
+
+    def Markdown(
+        self,
+        msgId: str,
+        recvId: str,
+        recvType: str,
+        content: str,
+        buttons: list[list[dict[str, any]]] = [],
+    ) -> dict[str, any]:
+        return self.NetJsonPost(
+            self.apiUrl,
+            self._gen_body(
+                msgId,
+                recvId,
+                recvType,
+                "markdown",
+                {"text": content, "buttons": buttons},
+            ),
+        )
