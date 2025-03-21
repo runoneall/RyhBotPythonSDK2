1b3e3e7af1e45b9a2f62e5ce2fe60a7f50cb8996
runoneall <runoobsteve@hotmail.com>
2025-03-21 10:16:30 +0800

    update

--- modules/m_Server/s.py
+++ modules/m_Server/s.py
@@ -21,7 +21,7 @@ class Server:
         def Handle():
             data = flask.request.json
             t_name = data["header"]["eventType"]
-            if t_name not in self.triggers:
+            if t_name not in self.triggers or len(self.triggers[t_name]) == 0:
                 return "IGNORE"
             for h in self.triggers[t_name]:
                 h.OnRecv(data)
