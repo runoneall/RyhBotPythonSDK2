771fef1c1077f803d676b9d098cdacd13dbda92f
runoneall <runoobsteve@hotmail.com>
2025-03-30 08:30:35 +0800

    update

--- modules/m_ServCommand/t.py
+++ modules/m_ServCommand/t.py
@@ -1,11 +1,19 @@
 class Trigger:
     def __init__(self, sdk):
         self.on = "message.receive.instruction"
-        self.handles: list[object] = []
+        self.handles: dict[list[object]] = {"ALL": []}
 
-    def AddHandle(self, handle):
-        self.handles.append(handle)
+    def AddHandle(self, handle, cmdid="ALL"):
+        if cmdid not in self.handles:
+            self.handles[cmdid] = []
+        self.handles[cmdid].append(handle)
 
     def OnRecv(self, data):
-        for handle in self.handles:
+        if "instructionId" in data["event"]["message"]:
+            cmdid = data["event"]["message"]["instructionId"]
+        else:
+            cmdid = data["event"]["message"]["commandId"]
+        for handle in self.handles.get(cmdid, []):
+            handle(data)
+        for handle in self.handles.get("ALL", []):
             handle(data)
