76736eb7f993c15e33303e0f9f37ca0a5f8fdb9f
runoneall <runoobsteve@hotmail.com>
2025-03-31 15:58:13 +0800

    update

--- sdk.py
+++ sdk.py
@@ -2,21 +2,31 @@ if __name__ == "__main__":
 
     import os
     import util
+    import json
 
     sdkModulePath = os.path.join(os.path.dirname(__file__), "modules")
     CmdArg = util.CmdArg()
 
-    def on_set_a(value):
-        print("set a", value)
-    
-    def on_set_b(value):
-        print("set b", value)
-    
-    def on_set_c(value):
-        print("set c", value)
-    
-    CmdArg.Bind("--a", on_set_a)
-    CmdArg.Bind("--b", on_set_b)
-    CmdArg.Bind("--c", on_set_c)
+    def checkEnvFile():
+        if not os.path.exists("./env.json"):
+            with open("./env.json", "w") as f:
+                f.write("{}")
+
+    def getEnvFile():
+        with open("./env.json", "r") as f:
+            return json.load(f)
+
+    def getEnv(value):
+        checkEnvFile()
+        print(getEnvFile().get(value, None))
+
+    CmdArg.Bind("-get-env", getEnv)
+
+    def listEnv(value):
+        checkEnvFile()
+        for key, value in getEnvFile().items():
+            print(key, "->", value)
+
+    CmdArg.Bind("-list-env", listEnv)
 
     CmdArg.Execute()
