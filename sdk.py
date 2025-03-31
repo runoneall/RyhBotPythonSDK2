4a3d815e2a5eb9873ee9f35160c4c830cf11e02b
runoneall <runoobsteve@hotmail.com>
2025-03-31 16:22:35 +0800

    update

--- sdk.py
+++ sdk.py
@@ -16,6 +16,10 @@ if __name__ == "__main__":
         with open("./env.json", "r") as f:
             return json.load(f)
 
+    def writeEnvFile(envObj):
+        with open("./env.json", "w") as f:
+            json.dump(envObj, f, indent=2)
+
     def getEnv(value):
         checkEnvFile()
         print(getEnvFile().get(value, None))
@@ -28,5 +32,38 @@ if __name__ == "__main__":
             print(key, "->", value)
 
     CmdArg.Bind("-list-env", listEnv)
+
+    def setEnv(value):
+        checkEnvFile()
+        k = value.split("=")[0]
+        v = value.split("=")[1]
+        if ":" in v:
+            v_type = v.split(":")[0]
+            v = v.split(":")[1]
+        else:
+            v_type = "str"
+
+        if v_type == "int":
+            v = int(v)
+        elif v_type == "str":
+            v = str(v)
+        elif v_type == "bool":
+            if v == "true":
+                v = True
+            else:
+                v = False
+        else:
+            v = float(v)
+        writeEnvFile({**getEnvFile(), **{k: v}})
+
+    CmdArg.Bind("-set-env", setEnv)
+
+    def delEnv(value):
+        checkEnvFile()
+        origin_env = getEnvFile()
+        del origin_env[value]
+        writeEnvFile(origin_env)
+
+    CmdArg.Bind("-del-env", delEnv)
 
     CmdArg.Execute()
