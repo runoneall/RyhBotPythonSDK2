72f07cc17316ed9ada346ef6f90e4ea752dbb615
runoneall <runoobsteve@hotmail.com>
2025-04-02 15:40:04 +0800

    update

--- modules/m_OpenAI/ai.py
+++ modules/m_OpenAI/ai.py
@@ -15,12 +15,12 @@ class OpenAI:
         self.aisHistory[name] = []
         self.logger.info(f"New AI: {name}")
 
-    def Generate(self, name, modelName, prompt, **kwargs):
+    def Generate(self, name, prompt, **kwargs):
         self.aisHistory[name].append({"role": "user", "content": prompt})
         self.logger.info(f"Generating for {name}: {prompt}")
         tokens = []
         for chunk in self.ais[name].chat.completions.create(
-            model=modelName,
+            model=name,
             messages=self.aisHistory[name],
             stream=True,
             **kwargs,
