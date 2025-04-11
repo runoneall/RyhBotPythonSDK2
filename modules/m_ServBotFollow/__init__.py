3ab589e31baa85e20befceab7ce02de43ef9eec8
runoneall <runoobsteve@hotmail.com>
2025-04-11 19:52:29 +0800

    update

---
+++ modules/m_ServBotFollow/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "ServBotFollow",
+    "version": "1.0.0",
+    "description": "云湖SDK服务器机器人关注消息处理模块",
+    "author": "r1a",
+    "dependencies": ["m_Server"],
+}
+
+from .t import Trigger as Main
