be774edf37e4fb238adf7f099abe9b80d573222c
runoneall <runoobsteve@hotmail.com>
2025-04-11 21:54:18 +0800

    update

---
+++ modules/m_ServBotUnFollow/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "ServBotUnFollow",
+    "version": "1.0.0",
+    "description": "云湖SDK服务器机器人取关消息处理模块",
+    "author": "r1a",
+    "dependencies": ["m_Server"],
+}
+
+from .t import Trigger as Main
