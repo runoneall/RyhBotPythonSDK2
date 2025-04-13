8881f2b01aec3d2565fd254e6f295ae95ecd52bf
runoneall <runoobsteve@hotmail.com>
2025-04-13 16:37:18 +0800

    update

---
+++ modules/m_ServGroupJoin/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "ServGroupJoin",
+    "version": "1.0.0",
+    "description": "云湖SDK服务器群组入群消息处理模块",
+    "author": "r1a",
+    "dependencies": ["m_Server"],
+}
+
+from .t import Trigger as Main
---
+++ modules/m_ServGroupJoin/t.py
@@ -0,0 +1,18 @@
+class Trigger:
+    def __init__(self, sdk, logger):
+        self.logger = logger
+        self.on = "group.join"
+        self.handles: dict[list[object]] = {"ALL": []}
+
+    def AddHandle(self, handle, uid="ALL"):
+        self.logger.info(f"Add Handler {handle.__name__} With UID {uid}")
+        if uid not in self.handles:
+            self.handles[uid] = []
+        self.handles[uid].append(handle)
+
+    def OnRecv(self, data):
+        uid = data["event"]["userId"]
+        for handle in self.handles.get(uid, []):
+            handle(data)
+        for handle in self.handles.get("ALL", []):
+            handle(data)
