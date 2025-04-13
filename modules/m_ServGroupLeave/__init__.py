c77200214e08055884ac457460f06f543869a0af
runoneall <runoobsteve@hotmail.com>
2025-04-13 16:42:34 +0800

    update

---
+++ modules/m_ServGroupLeave/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "ServGroupLeave",
+    "version": "1.0.0",
+    "description": "云湖SDK服务器群组离群消息处理模块",
+    "author": "r1a",
+    "dependencies": ["m_Server"],
+}
+
+from .t import Trigger as Main
