b71e47afa74d8a29369afb2474888d1f7e55578e
runoneall <runoobsteve@hotmail.com>
2025-04-13 17:31:43 +0800

    update

---
+++ modules/m_ServButtonClick/__init__.py
@@ -0,0 +1,9 @@
+moduleInfo = {
+    "name": "ServButtonClick",
+    "version": "1.0.0",
+    "description": "云湖SDK服务器按钮点击消息处理模块",
+    "author": "r1a",
+    "dependencies": ["m_Server"],
+}
+
+from .t import Trigger as Main
